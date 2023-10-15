0e6b2b4479c20b1119a8138dd6f7db6b2132e7e3
vyatkinsen <vyatkinsen@bk.ru>
2023-10-15 18:48:41 +0300

    added model

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -2,12 +2,9 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
-    let restaurantNames = [
-        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
-        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
-        "Speak Easy", "Morris Pub", "Вкусные истории",
-        "Классик", "Love&Life", "Шок", "Бочка"
-    ]
+
+    
+    let places = Place.getPlaces()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,25 +13,22 @@ class MainViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.nameLabel?.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel?.text = places[indexPath.row].name
+        cell.locationLabel?.text = places[indexPath.row].location
+        cell.typeLabel?.text = places[indexPath.row].type
+        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.bounds.height / 2
         cell.imageOfPlace?.clipsToBounds = true
 
         return cell
     }
     
-    // MARK: - Table View Delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
-    }
     
     // MARK: - Navigation
 
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+ struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        var places = [Place] ()
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Спб", type: "Ресторан", image: place))
+        }
+        return places
+    }
+}
