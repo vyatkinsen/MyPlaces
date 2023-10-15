cf730fc7d4c61b0ebccda568821d4e0cb48cd96d
vyatkinsen <vyatkinsen@bk.ru>
2023-10-15 17:17:48 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Арсений Вяткин on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
